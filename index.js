0154f5cfe519b560ab1931c05b2d11077848e2ab
CTAPbIuMABP <trejgun@gmail.com>
2016-03-13 17:08:43 +0800

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+var moment = require("moment");
+var range = require("moment-range");
+var timezone = require("moment-timezone");
+
+range.prototype.contains = function contains(other, exclusive) {
+	const start = this.start;
+	const end = this.end;
+
+	if (other instanceof range) {
+		return (start < other.start || (start.isSame(other.start) && !exclusive)) && (end > other.end || (end.isSame(other.end) && !exclusive));
+	} else {
+		return (start < other || (start.isSame(other) && !exclusive)) && (end > other || (end.isSame(other) && !exclusive));
+	}
+};
+
+exports.default = moment;
